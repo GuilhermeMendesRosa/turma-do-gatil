aed73b2bb3f08eebfc9700d8ac483b634abd7cea
Guilherme Mendes <83733948+GuilhermeMendesRosa@users.noreply.github.com>
2025-08-28 13:42:23 -0300

    backend

---
+++ turma-do-gatil-back/src/main/java/br/com/udesc/turma_do_gatil_back/TurmaDoGatilBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.udesc.turma_do_gatil_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurmaDoGatilBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurmaDoGatilBackApplication.class, args);
+	}
+
+}
---
+++ turma-do-gatil-back/src/test/java/br/com/udesc/turma_do_gatil_back/TurmaDoGatilBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udesc.turma_do_gatil_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurmaDoGatilBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
