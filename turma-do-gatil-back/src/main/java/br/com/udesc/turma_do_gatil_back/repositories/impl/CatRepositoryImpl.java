3c6396092e4e6f5ac70efe797a5a71c7ef57d3b7
Guilherme Mendes <83733948+GuilhermeMendesRosa@users.noreply.github.com>
2025-10-30 20:26:56 -0300

    Update CatRepositoryImpl.java

--- turma-do-gatil-back/src/main/java/br/com/udesc/turma_do_gatil_back/repositories/impl/CatRepositoryImpl.java
+++ turma-do-gatil-back/src/main/java/br/com/udesc/turma_do_gatil_back/repositories/impl/CatRepositoryImpl.java
@@ -7,15 +7,18 @@ import br.com.udesc.turma_do_gatil_back.enums.Color;
 import br.com.udesc.turma_do_gatil_back.enums.Sex;
 import br.com.udesc.turma_do_gatil_back.repositories.CatRepositoryCustom;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -47,8 +50,15 @@ public class CatRepositoryImpl implements CatRepositoryCustom {
         }
 
         JPAQuery<Cat> query = queryFactory.selectFrom(qCat)
-                .where(predicate)
-                .orderBy(qCat.name.asc());
+                .where(predicate);
+
+        // Aplicar ordenação dinâmica
+        OrderSpecifier<?>[] orderSpecifiers = getOrderSpecifiers(pageable.getSort());
+        if (orderSpecifiers.length > 0) {
+            query.orderBy(orderSpecifiers);
+        } else {
+            query.orderBy(qCat.name.asc());
+        }
 
         long total = query.fetchCount();
 
@@ -63,8 +73,15 @@ public class CatRepositoryImpl implements CatRepositoryCustom {
     @Override
     public Page<Cat> findByAdoptionStatus(CatAdoptionStatus adoptionStatus, Pageable pageable) {
         JPAQuery<Cat> query = queryFactory.selectFrom(qCat)
-                .where(qCat.adoptionStatus.eq(adoptionStatus))
-                .orderBy(qCat.name.asc());
+                .where(qCat.adoptionStatus.eq(adoptionStatus));
+
+        // Aplicar ordenação dinâmica
+        OrderSpecifier<?>[] orderSpecifiers = getOrderSpecifiers(pageable.getSort());
+        if (orderSpecifiers.length > 0) {
+            query.orderBy(orderSpecifiers);
+        } else {
+            query.orderBy(qCat.name.asc());
+        }
 
         long total = query.fetchCount();
 
@@ -87,8 +104,15 @@ public class CatRepositoryImpl implements CatRepositoryCustom {
     @Override
     public Page<Cat> findByColor(Color color, Pageable pageable) {
         JPAQuery<Cat> query = queryFactory.selectFrom(qCat)
-                .where(qCat.color.eq(color))
-                .orderBy(qCat.name.asc());
+                .where(qCat.color.eq(color));
+
+        // Aplicar ordenação dinâmica
+        OrderSpecifier<?>[] orderSpecifiers = getOrderSpecifiers(pageable.getSort());
+        if (orderSpecifiers.length > 0) {
+            query.orderBy(orderSpecifiers);
+        } else {
+            query.orderBy(qCat.name.asc());
+        }
 
         long total = query.fetchCount();
 
@@ -103,8 +127,15 @@ public class CatRepositoryImpl implements CatRepositoryCustom {
     @Override
     public Page<Cat> findBySex(Sex sex, Pageable pageable) {
         JPAQuery<Cat> query = queryFactory.selectFrom(qCat)
-                .where(qCat.sex.eq(sex))
-                .orderBy(qCat.name.asc());
+                .where(qCat.sex.eq(sex));
+
+        // Aplicar ordenação dinâmica
+        OrderSpecifier<?>[] orderSpecifiers = getOrderSpecifiers(pageable.getSort());
+        if (orderSpecifiers.length > 0) {
+            query.orderBy(orderSpecifiers);
+        } else {
+            query.orderBy(qCat.name.asc());
+        }
 
         long total = query.fetchCount();
 
@@ -119,8 +150,15 @@ public class CatRepositoryImpl implements CatRepositoryCustom {
     @Override
     public Page<Cat> findByNameContainingIgnoreCase(String name, Pageable pageable) {
         JPAQuery<Cat> query = queryFactory.selectFrom(qCat)
-                .where(qCat.name.containsIgnoreCase(name))
-                .orderBy(qCat.name.asc());
+                .where(qCat.name.containsIgnoreCase(name));
+
+        // Aplicar ordenação dinâmica
+        OrderSpecifier<?>[] orderSpecifiers = getOrderSpecifiers(pageable.getSort());
+        if (orderSpecifiers.length > 0) {
+            query.orderBy(orderSpecifiers);
+        } else {
+            query.orderBy(qCat.name.asc());
+        }
 
         long total = query.fetchCount();
 
@@ -137,5 +175,44 @@ public class CatRepositoryImpl implements CatRepositoryCustom {
         return queryFactory.selectFrom(qCat)
                 .where(qCat.adoptionStatus.eq(adoptionStatus))
                 .fetchCount();
+    }
+
+    /**
+     * Converte Sort do Spring para OrderSpecifier do QueryDSL
+     */
+    private OrderSpecifier<?>[] getOrderSpecifiers(Sort sort) {
+        List<OrderSpecifier<?>> orders = new ArrayList<>();
+
+        for (Sort.Order order : sort) {
+            String property = order.getProperty();
+            boolean isAscending = order.isAscending();
+
+            switch (property) {
+                case "name":
+                    orders.add(isAscending ? qCat.name.asc() : qCat.name.desc());
+                    break;
+                case "birthDate":
+                    orders.add(isAscending ? qCat.birthDate.asc() : qCat.birthDate.desc());
+                    break;
+                case "color":
+                    orders.add(isAscending ? qCat.color.asc() : qCat.color.desc());
+                    break;
+                case "sex":
+                    orders.add(isAscending ? qCat.sex.asc() : qCat.sex.desc());
+                    break;
+                case "adoptionStatus":
+                    orders.add(isAscending ? qCat.adoptionStatus.asc() : qCat.adoptionStatus.desc());
+                    break;
+                case "shelterEntryDate":
+                    orders.add(isAscending ? qCat.shelterEntryDate.asc() : qCat.shelterEntryDate.desc());
+                    break;
+                default:
+                    // Se a propriedade não for reconhecida, ordena por nome
+                    orders.add(qCat.name.asc());
+                    break;
+            }
+        }
+
+        return orders.toArray(new OrderSpecifier<?>[0]);
     }
 }
