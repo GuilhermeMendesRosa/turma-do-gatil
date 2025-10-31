78c6f4be97680bce31aacf6719bd080f82706664
Guilherme Mendes <83733948+GuilhermeMendesRosa@users.noreply.github.com>
2025-10-31 20:09:09 -0300

    Update S3Service.java

--- turma-do-gatil-back/src/main/java/br/com/udesc/turma_do_gatil_back/services/S3Service.java
+++ turma-do-gatil-back/src/main/java/br/com/udesc/turma_do_gatil_back/services/S3Service.java
@@ -14,7 +14,6 @@ import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequ
 
 import java.io.IOException;
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -76,7 +75,7 @@ public class S3Service {
                     .build();
 
             GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                    .signatureDuration(Duration.of(100, ChronoUnit.YEARS))
+                    .signatureDuration(Duration.ofDays(99999))
                     .getObjectRequest(getObjectRequest)
                     .build();
 
