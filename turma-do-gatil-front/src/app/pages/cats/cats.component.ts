214a60183f6c8b24307d4b3e9964b7b4037e4d27
Guilherme Mendes Rosa <83733948+GuilhermeMendesRosa@users.noreply.github.com>
2025-12-03 19:24:40 -0300

    Revert "ajuste"
    This reverts commit 4fdf00d4d8a0a2a61f545a3f4629e46b74bbd9b6.

--- turma-do-gatil-front/src/app/pages/cats/cats.component.ts
+++ turma-do-gatil-front/src/app/pages/cats/cats.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { Select } from 'primeng/select';
 import { TagModule } from 'primeng/tag';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TooltipModule } from 'primeng/tooltip';
@@ -65,6 +66,7 @@ import {
     CardModule,
     ButtonModule,
     InputTextModule,
+    Select,
     TagModule,
     SkeletonModule,
     TooltipModule,
